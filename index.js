ad95a9d8560ad3d5a82a99628ba1d2bcb9ff9a31
George Suarez <georgejsuarez@gmail.com>
2023-09-25 20:01:15 -0700

    Finished implementing the 3 incomplete functions

--- index.js
+++ index.js
@@ -5,7 +5,10 @@
  * @param {string} fahren temperature in degrees F
  * @returns {number} the number of degrees C
  */
-function convertToCelsius(fahren) {}
+function convertToCelsius(fahren) {
+  let fahrenInt = parseInt(fahren);
+  return (fahrenInt - 32) * 5 / 9;
+}
 
 /**
  * Takes both numbers (F, C) and display a message with
@@ -22,14 +25,26 @@ function convertToCelsius(fahren) {}
  * @param {number} fahren
  * @param {number} celc
  */
-function createMessage(fahren, celc) {}
+function createMessage(fahren, celc) {
+  if (fahren <= 32) {
+    return `temperature is ${fahren}°F and ${celc}°C and feels very cold`;
+  } else if (fahren <= 64) {
+    return `temperature is ${fahren}°F and ${celc}°C and feels cold`;
+  } else if (fahren <= 86) {
+    return `temperature is ${fahren}°F and ${celc}°C and feels warm`;
+  } else if (fahren >= 100) {
+    return `temperature is ${fahren}°F and ${celc}°C and feels hot`;
+  }
+}
 
 /**
  * Takes a number and returns a random integer from 0 to the limit
  * @param {number} limit
  * @returns {number} a number between 0 and the int passed in
  */
-function rand(limit) {}
+function rand(limit) {
+  return Math.round(Math.random() * limit);
+}
 
 // -------------------- DONT NOT CHANGE THE CODE BELOW ---------------------- //
 
